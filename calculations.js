496399720afaf0a81bbe5ed0155e2ae33de1ad53
jbakerbba <jbakerbba2@gmail.com>
2023-09-06 13:44:23 -0400

    commit

---
+++ calculations.js
@@ -0,0 +1,31 @@
+p//1
+var ticketNum = 2;
+var ticketCost = 14.00;
+var totalTicketCost = ticketNum * ticketCost;
+
+document.getElementById('ticketNum').innerHTML = "Tickets" + " " + ticketNum;
+document.getElementById('ticketCost').innerHTML= "Cost: $" + ticketCost;
+document.getElementById('ttCost').innerHTML= "Total: $" + ticketCost * ticketNum;
+
+//2
+var AccountNum = 235.87;
+var Sweater = 55;
+var Pants = 75;
+var Shoes = 60;
+var Jacket = 70;
+
+var Outfit = Sweater + Pants + Shoes + Jacket;
+document.write("<p>Do you have enough funds?:" + (AccountNum > Outfit) + "</p>")
+
+
+var pizza = 4;
+var studentEats = 2.5;
+var pizzaSlices = 8;
+Math.round('profPizza' * pizzaSlices )
+document.getElementById(math.round('profPizza')).innerhtml = pizzaSlices * pizza / studentEats;
+
+
+
+
+
+
